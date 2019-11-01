09b24f8b3f5f629e84de1b40dbfbe982d232a320
Dave Carlson <dave.carlson@tr.com>
2019-11-01 14:04:10 -0500

    Initial Commit

---
+++ src/main/java/com/bixbysdoghouse/bootfundamentalsdemo/BootFundamentalsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bixbysdoghouse.bootfundamentalsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFundamentalsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFundamentalsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bixbysdoghouse/bootfundamentalsdemo/BootFundamentalsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bixbysdoghouse.bootfundamentalsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootFundamentalsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
